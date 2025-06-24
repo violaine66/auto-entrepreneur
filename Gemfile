cf7bc8a28b45b459a05a8291814d81ee5fa29ecb
violaine soulas <vhsoulas@gmail.com>
2025-06-24 10:34:06 +0200

    Mise en page devise

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem "bootsnap", require: false
 
 gem "bootstrap", "~> 5.3"
 gem "devise"
+gem 'devise-i18n'
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module AutoEntrepreneur
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :fr
+
+     config.i18n.available_locales = [:fr, :en]
+
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
