22b4afeba3081566abe826e742fe2f02d1ac3923
violaine soulas <vhsoulas@gmail.com>
2025-06-24 10:07:49 +0200

    Mise en place sweet alert pour action delete

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -3,6 +3,30 @@ import Swal from "sweetalert2";
 
 // Connects to data-controller="alert"
 export default class extends Controller {
-  connect() {
+  static values = {
+    icon: String,
+    alertTitle: String,
+    alertHtml: String,
+    confirmButtonText: String,
+    showCancelButton: Boolean,
+    cancelButtonText: String,
+  };
+
+  initSweetalert(event) {
+    event.preventDefault(); // Prevent the form to be submited after the submit button has been clicked
+
+    Swal.fire({
+      icon: this.iconValue,
+      title: this.alertTitleValue,
+      html: this.htmlValue,
+      confirmButtonText: this.confirmButtonTextValue,
+      showCancelButton: this.showCancelButtonValue,
+      cancelButtonText: this.cancelButtonTextValue,
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        event.target.submit()
+      }// soumet le formulaire après confirmation
+      })
   }
 }
