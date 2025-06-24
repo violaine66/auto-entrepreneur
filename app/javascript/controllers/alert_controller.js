d1b72dacaa5ed628a26b2e7f16cb3eab204b7bf3
violaine soulas <vhsoulas@gmail.com>
2025-06-24 09:53:19 +0200

    Import SweetAlert

---
+++ app/javascript/controllers/alert_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2";
+
+// Connects to data-controller="alert"
+export default class extends Controller {
+  connect() {
+  }
+}
